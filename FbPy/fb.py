9858b4a1d095d07e4aed213dbd6738c1037738ca
Abhijeet <void.aby@gmail.com>
2014-05-11 22:10:40 +0530

    added friendlist option

--- FbPy/fb.py
+++ FbPy/fb.py
@@ -5,8 +5,13 @@ oauth_access_token = "CAACEdEose0cBAO04waCG0vfZCOIfloedhMHzMZBZB56FBhv52GwTZCMAe
 
 graph = facebook.GraphAPI(oauth_access_token)
 profile = graph.get_object("me")
+friends = graph.get_connections("me", "friends")
 
-
+print "Welcome to Facebook!"
 print profile["first_name"]+profile["last_name"]
 print profile["gender"]
+
+print "Your friendlist"
+for friend in friends["data"]:
+	print friend["name"]
 
