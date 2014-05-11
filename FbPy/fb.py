b40af592eecb6f84e4bdcd5983daad5e2494ebc8
Abhijeet <void.aby@gmail.com>
2014-05-11 22:04:11 +0530

    added fb api with access token

---
+++ FbPy/fb.py
@@ -0,0 +1,12 @@
+
+import facebook
+
+oauth_access_token = "CAACEdEose0cBAO04waCG0vfZCOIfloedhMHzMZBZB56FBhv52GwTZCMAer4kZAiSdxz5Ap66fLjX18erhGHwUm5qx2ZBtVVt4LZA4YqpDoLP8XKJq9PVGArUmZBqcZAu2eQz2UdqObnFxI3thDN5TCDTarkZA3sQblWN1ZAEbCot2QT6JICw4btpQB8Q2PjvZCmnDU8ZD"
+
+graph = facebook.GraphAPI(oauth_access_token)
+profile = graph.get_object("me")
+
+
+print profile["first_name"]+profile["last_name"]
+print profile["gender"]
+
