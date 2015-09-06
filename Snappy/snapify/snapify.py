e0be455c5cff8c1c352729ef18031c679874722c
Abhijeet Mohan <void.aby@gmail.com>
2015-09-06 18:39:03 +0530

    Update snapify.py

--- Snappy/snapify/snapify.py
+++ Snappy/snapify/snapify.py
@@ -33,7 +33,8 @@ def clip(url,format,w,h):
 @click.argument('format',help='extension of snapshot image',default='png')
 @click.argument('w',help='width of snapshot image',default='1024')
 @click.argument('h',help='height of snapshot image',default='800')
-def web(url,format,w,h):	
+def web(url,format,w,h):
+	# create a screenshot
 	response = unirest.get("https://jmillerdesign-url-screenshot.p.mashape.com/api?url="+urllib.quote(url)+"&format="+format+"&width="+w+"&height="+h+"&delay=5",
 	  headers={
 		"X-Mashape-Authorization": "7cu8k0bYarwTOOPnOFgJQn9nWwt4tr33"
