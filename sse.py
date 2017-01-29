9934ea9be984fe096887e0c66001de8cf268d19a
Abhijeet Mohan <void.aby@gmail.com>
2017-01-29 23:59:07 +0530

    Add sse example

---
+++ sse.py
@@ -0,0 +1,60 @@
+from gevent import monkey; monkey.patch_all()
+from gevent import sleep
+
+from bottle import get, post, request, response
+from bottle import GeventServer, run
+
+
+sse_test_page = """
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js "></script>
+		<script>
+			$(document).ready(function() {
+				var es = new EventSource("stream");
+				es.onmessage = function (e) {
+					$("#log").html($("#log").html()
+						+ "<p>Event: " + e.event + ", data: " + e.data + "</p>");
+				};
+			})
+		</script>
+	</head>
+	<body>
+		<div id="log" style="font-family: courier; font-size: 0.75em;"></div>
+	</body>
+</html>
+"""
+
+
+@get('/')
+def index():
+	return sse_test_page
+
+
+@get('/stream')
+def stream():
+	# "Using server-sent events"
+	# https://developer.mozilla.org/en-US/docs/Server-sent_events/Using_server-sent_events
+	# "Stream updates with server-sent events"
+	# http://www.html5rocks.com/en/tutorials/eventsource/basics/
+
+	response.content_type  = 'text/event-stream'
+	response.cache_control = 'no-cache'
+
+	# Set client-side auto-reconnect timeout, ms.
+	yield 'retry: 100\n\n'
+
+	n = 1
+
+	# Keep connection alive no more then... (s)
+	end = time.time() + 60
+	while time.time() < end:
+		yield 'data: %i\n\n' % n
+		n += 1
+		sleep(1)
+
+
+if __name__ == '__main__':
+	run(server=GeventServer)
