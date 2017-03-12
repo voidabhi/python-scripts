feafbefbc7de1f71cfa50af51139c4a09a306012
Abhijeet Mohan <void.aby@gmail.com>
2017-03-12 23:55:34 +0530

    Add socket io client example

---
+++ socketio-client.py
@@ -0,0 +1,49 @@
+import websocket, httplib, sys, asyncore
+
+'''
+    connect to the socketio server
+    1. perform the HTTP handshake
+    2. open a websocket connection '''
+def connect(server, port):
+    
+    print("connecting to: %s:%d" %(server, port))
+    
+    conn  = httplib.HTTPConnection(server + ":" + str(port))
+    conn.request('POST','/socket.io/1/')
+    resp  = conn.getresponse() 
+    hskey = resp.read().split(':')[0]
+
+    ws = websocket.WebSocket(
+                    'ws://'+server+':'+str(port)+'/socket.io/1/websocket/'+hskey,
+                    onopen   = _onopen,
+                    onmessage = _onmessage,
+                    onclose = _onclose)
+    
+    return ws
+
+def _onopen():
+    print("opened!")
+
+def _onmessage(msg):
+    print("msg: " + str(msg))
+
+def _onclose():
+    print("closed!")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        sys.stderr.write('usage: python client.py <server> <port>\n')
+        sys.exit(1)
+    
+    server = sys.argv[1]
+    port = int(sys.argv[2])
+    
+    ws = connect(server, port)
+    
+    try:
+        asyncore.loop()
+    except KeyboardInterrupt:
+        ws.close()
+    
+    
