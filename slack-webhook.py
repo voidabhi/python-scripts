9ee6851753ae0767b8709e364f0c02fa7da61554
Abhijeet Mohan <void.aby@gmail.com>
2017-03-29 23:37:54 +0530

    Add slack webhook

---
+++ slack-webhook.py
@@ -0,0 +1,16 @@
+import json
+import requests
+
+# Set the webhook_url to the one provided by Slack when you create the webhook at https://my.slack.com/services/new/incoming-webhook/
+webhook_url = 'https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX'
+slack_data = {'text': "Sup! We're hacking shit together @HackSussex :spaghetti:"}
+
+response = requests.post(
+    webhook_url, data=json.dumps(slack_data),
+    headers={'Content-Type': 'application/json'}
+)
+if response.status_code != 200:
+    raise ValueError(
+        'Request to slack returned an error %s, the response is:\n%s'
+        % (response.status_code, response.text)
+    )
