7771baf7b4806c15cb85df6c34e344345538df4a
Abhijeet Mohan <void.aby@gmail.com>
2017-05-10 23:56:56 +0530

    Add background thread example

---
+++ background-thread.py
@@ -0,0 +1,15 @@
+import time
+import requests
+
+from tomorrow import threads
+
+@threads(5)
+def download(url):
+    return requests.get(url)
+
+if __name__ == "__main__":
+    start = time.time()
+    responses = [download(url) for url in urls]
+    html = [response.text for response in responses]
+    end = time.time()
+    print "Time: %f seconds" % (end - start)
