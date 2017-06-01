ef561347e3f51ce811e61cd8b0048fc84fdf2493
Abhijeet Mohan <void.aby@gmail.com>
2017-06-01 23:47:02 +0530

    Update trie implementation

--- trie.py
+++ trie.py
@@ -1,74 +1,42 @@
-class Node:
+
+class TrieNode(object):
+    
+    def __init__(self, c):
+        self.map = {}
+        self.character = c
+        self.isLast = False
+
+class Trie(object):
+
     def __init__(self):
-        self.next = {}
-        self.word_marker = False 
+        self.root = TrieNode('')
 
+    def insert(self, word):
+        curr = self.root
+        for c in word:
+            if c not in curr.map:
+                curr.map[c] = TrieNode(c)
+            curr = curr.map[c]
+        curr.isLast = True
 
-    def add(self, string):
-        
-        if len(string) == 0:
-            self.word_marker = True 
-            return 
-        
-        key = string[0] 
-        string = string[1:] 
+    def search(self, word):
+        curr = self.root
+        for c in word:
+            if c not in curr.map:
+                return False
+            curr = curr.map[c]
+        return curr.isLast
 
-        if self.next.has_key(key):
-            self.next[key].add(string)
-        else:
-            node = Node()
-            self.next[key] = node
-            node.add(string)
+    def startsWith(self, prefix):
+        curr = self.root
+        for c in prefix:
+            if c not in curr.map:
+                return False
+            curr = curr.map[c]
+        return True
 
-
-    def partial(self, sf=None):
-        
-        if self.next.keys() == []:
-            print "Match:",sf
-            return
-            
-        if self.word_marker == True:
-            print "Match:",sf
-
-        for key in self.next.keys():
-            self.next[key].partial(sf+key)
-
-    def search(self, string, sf=""):
-        if len(string) > 0:
-            key = string[0]
-            string = string[1:]
-            if self.next.has_key(key):
-                sf = sf + key
-                self.next[key].search(string,sf)
-                
-            else:
-                print "No match"
-        else:
-            if self.word_marker == True:
-                print "Match:",sf
-
-            for key in self.next.keys():
-                self.next[key].partial(sf+key)
-
-    def exact_match(self, string, sf=''):
-        if len(string) > 0:
-            key = string[0]
-            string = string[1:]
-            if self.next.has_key(key):
-                sf = sf + key
-                self.next[key].exact_match(string,sf)
-                
-            else:
-                print "No match"
-        else:
-            if self.word_marker == True:
-                print "Match:",sf
-            elif self.word_marker==False:
-                print 'No Match.'
-
-    def all_trie(self):
-        a=sorted(self.next.keys())
-        if(len(a)==0):
-            print 'Empty Trie.'
-        for b in a:
-            self.next[b].partial(b)
+if __name__ == '__main__':
+    t = Trie()
+    t.insert('somestring')
+    print t.search('some')
+    print t.startsWith('some')
