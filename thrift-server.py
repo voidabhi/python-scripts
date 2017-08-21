2399d31b2901b8be219f462dc7063e404ef83685
Abhijeet Mohan <void.aby@gmail.com>
2017-08-20 23:45:56 -0700

    Add thrift server

---
+++ thrift-server.py
@@ -0,0 +1,25 @@
+
+
+
+"""
+
+pingpong.thrift
+---------------
+
+service PingPong {
+    string ping(),
+}
+"""
+
+
+import thriftpy
+pingpong_thrift = thriftpy.load("pingpong.thrift", module_name="pingpong_thrift")
+
+from thriftpy.rpc import make_server
+
+class Dispatcher(object):
+    def ping(self):
+        return "pong"
+
+server = make_server(pingpong_thrift.PingPong, Dispatcher(), '127.0.0.1', 6000)
+server.serve()
